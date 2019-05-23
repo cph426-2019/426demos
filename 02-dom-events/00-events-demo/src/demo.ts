0862041c9c322550b50075ecdf76f444608d21ad
Kris Jordan <krisjordan@gmail.com>
2019-05-23 13:57:26 +0200

    Update demo

--- 02-dom-events/00-events-demo/src/demo.ts
+++ 02-dom-events/00-events-demo/src/demo.ts
@@ -1,15 +1,5 @@
 let main = () => {
-    let all = document.querySelectorAll("*");
-    all.forEach((element) => element.addEventListener('click', handleClick));
-};
-
-let handleClick = function(e: MouseEvent) {
-    console.log(this.tagName);
-    if (this.tagName === "HTML") {
-        console.log("=====");
-    }
-    this.style.border = "1px solid red";
-    this.style.background = "rgba(0, 0, 0, 0.1)";
+    console.log("Let's learn about events!");
 };
 
 window.addEventListener("load", main);
