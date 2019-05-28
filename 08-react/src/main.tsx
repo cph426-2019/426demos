74751a20d70fad1732f0f61c4e6eed5c415b5b28
Kris Jordan <krisjordan@gmail.com>
2019-05-28 12:44:37 +0200

    Update react basic demo

---
+++ 08-react/src/main.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+export let main = () => {
+    // Establish Target Element in DOM
+    // Render JSX using ReactDOM's render Method
+};
+window.addEventListener("load", main);
